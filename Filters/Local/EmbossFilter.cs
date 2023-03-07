5a59d567943ee364dd884f9f1f15af4c904f1192
Nikita Kudinov <kudinov.nikita@gmail.com>
2023-03-07 13:41:24 +0300

    feat: Add `EmbossFilter`

---
+++ Filters/Local/EmbossFilter.cs
@@ -0,0 +1,29 @@
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using ComputerGraphics0.Filters.Pixel;
+
+namespace ComputerGraphics0.Filters.Kernel;
+
+public class EmbossFilter : ConvolutionFilter
+{
+    public override string Name => "emboss";
+
+    private IncreaseBrightnessFilter _brighten;
+
+    public EmbossFilter()
+    {
+        _kernel = new[,]
+        {
+            {0f, 1f, 0f},
+            {1f, 0f, -1f},
+            {0f, -1f, 0f}
+        };
+
+        _brighten = new IncreaseBrightnessFilter();
+    }
+
+    public override Image<Argb32> Process(Image<Argb32> source)
+    {
+        return _brighten.Process(base.Process(source));
+    }
+}
